0350e2d611e43e76b9814612dba199a10d3c459a
Perry Auld <pjauld@Perrys-MBP.home>
2019-12-24 15:28:40 -0800

    Add index showcase

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,16 +4,63 @@
       <h1 class="title main">Learned Equine</h1>
       <h4 class="title sub">It's pronounced "Learn-ed"</h4>
     </div>
-    <!-- <div class="content">
+    <div class="content">
       <div class="text">
-
+        <div class="showcase_grid">
+          <div class="showcase1 grid_item img_wrapper">
+            <g-image class="img" src="../assets/images/kellie.jpg" />
+          </div>
+          <div class="showcase2 grid_item img_wrapper">
+            <g-image class="img" src="../assets/images/lita_3.jpg" />
+          </div>
+          <div class="showcase3 grid_item img_wrapper">
+            <g-image class="img reposition" src="../assets/images/obi_2.jpg"/>
+          </div>
+          <div class="showcase4 grid_item img_wrapper">
+            <g-image class="img" src="../assets/images/parker_jovi_2.jpg" />
+          </div>
+          <div class="showcase5 grid_item img_wrapper">
+            <g-image class="img" src="../assets/images/pumpkin.jpg" />
+          </div>
+          <div class="showcase6 grid_item img_wrapper">
+            <g-image class="img" src="../assets/images/lita_2.jpg" />
+          </div>
+        </div>
+        <h2>Recent Posts</h2>
+        <ul>
+          <BlogPost
+              v-for="edge in $page.allPosts.edges"
+              :key="edge.node.id"
+              :id="edge.node.id"
+              :title="edge.node.title"
+              :body="edge.node.body" />
+        </ul>
       </div>
-    </div> -->
+    </div>
   </Layout>
 </template>
 
+<page-query>
+  query {
+    allPosts(limit: 3) {
+      edges {
+        node {
+          id
+          title
+          body
+        }
+      }
+    }
+  }
+</page-query>
+
 <script>
+import BlogPost from '~/components/BlogPost.vue';
+
 export default {
+  components: {
+    BlogPost,
+  },
   metaInfo: {
     title: 'Home',
   }
@@ -21,12 +68,21 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  text-align: center;
+}
+
+ul {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
 .bg {
   background-image: url('../assets/background/index.jpg');
 }
 
 .content_title {
-  margin-bottom: 50vh;
+  margin-bottom: 55vh;
   position: relative;
   width: 100%;
 }
@@ -41,8 +97,43 @@ export default {
   font-style: italic;
 }
 
+.text {
+  padding: 5vh 5%;
+}
+
 .title {
   color: #FFFFFF;
   text-align: center;
+}
+
+.showcase_grid {
+  align-items: stretch;
+  display: grid;
+  grid-template-areas:
+    'a a a a b b b b c c'
+    'a a a a b b b b c c'
+    'a a a a d d e e f f'
+    'a a a a d d e e f f';
+  grid-gap: 2px;
+  margin-bottom: 7vh;
+}
+.grid_item {
+  display: flex;
+  align-items: center;
+}
+.showcase1 { grid-area: a; }
+.showcase2 { grid-area: b; }
+.showcase3 { grid-area: c; }
+.showcase4 { grid-area: d; }
+.showcase5 { grid-area: e; }
+.showcase6 { grid-area: f; }
+
+.img {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+.reposition {
+  object-position: 0;
 }
 </style>
