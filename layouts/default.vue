6a3421d38a41f649c06f72ed120499abb64fb6eb
Perry Auld <pjauld@Perrys-MacBook-Pro.local>
2021-06-07 11:34:49 -0700

    Add menu & index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,205 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="bg">
+    <header class="header">
+      <div id="logo">
+        <NuxtLink to="/"><h3>Learned Equine</h3></NuxtLink>
+      </div>
+      <div id="menu">
+        <input class="toggle" type="checkbox" />
+        <nav>
+          <NuxtLink class="nav_link" to="/">Book Club</NuxtLink>
+          <NuxtLink class="nav_link" to="/blog">Obi</NuxtLink>
+          <NuxtLink class="nav_link" to="/about">Bunny Fun</NuxtLink>
+          <NuxtLink class="nav_link" to="/contact">General Horse Knowledge and Tools</NuxtLink>
+        </nav>
+      </div>
+      <div class="mobile_menu">Menu</div>
+    </header>
+    <transition name="fade" appear>
+      <main>
+        <div class="body">
+          <Nuxt />
+        </div>
+      </main>
+    </transition>
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
+* {
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+body, html {
+  background-color: #F1EDE1;
+  height: 100%;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  line-height: 1.5;
+}
+a, a:hover, a:active, a:focus {
+  outline: 0;
   text-decoration: none;
-  padding: 10px 30px;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+h1 {
+  text-align: center;
+  margin-bottom: 5vh;
 }
 
-.button--grey {
-  display: inline-block;
+#logo {
+  padding: 18px 5% 18px 5%;
+}
+h3 {
+  color: #5E3B2A;
+  font-family: Garamond;
+  font-size: 2rem;
+  font-style: italic;
+}
+
+nav {
+  bottom: 0;
+  margin: 0 70px 15px 0;
+  position: absolute;
+  right: 0;
+}
+
+.body {
+  padding: 120px 10% 5% 10%;
+}
+
+.bg {
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100%;
+  position: relative;
+}
+.fade-enter-active {
+  transition: opacity .4s;
+}
+.fade-enter {
+  opacity: 0;
+}
+
+.content {
+  border-radius: 10px;
+  background-color: rgb(240, 231, 218, 0.9);
+  position: relative;
+  width: 100%;
+}
+
+.header {
+  background-color: #f1ede1;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  height: 85px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+
+.mobile_menu {
+  border: 1px solid #a39d94;
   border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+  color: #5E3B2A;
+  cursor: pointer;
+  display: none;
+  letter-spacing: 0.1rem;
+  padding: 10px;
+  position: absolute;
+  right: 4%;
+  top: 25%;
+}
+.toggle {
+  border: 0;
+  display: none;
+  height: 46px;
+  opacity: 0;
+  position: absolute;
+  right: 8%;
+  top: 25%;
+  width: 67px;
+  z-index: 2;
+}
+.mobile_menu:hover {
+  background-color: #089b06;
+  border: 1px solid #089b06;
+  color: #FFFFFF;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.nav_link {
+  border: 1px solid #F1EDE1;
+  border-radius: 0;
+  color: #5E3B2A;
+  cursor: pointer;
+  font-size: 0.8rem;
+  letter-spacing: 0.1rem;
+  margin-left: 2.5vw;
+  padding: 7px;
+  text-decoration: none;
+  z-index: 1;
+}
+.nav_link:hover {
+  background-color: #089b06;
+  border: 1px solid #089b06;
+  border-radius: 7%;
+  color: #FFFFFF;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
+}
+
+.text {
+  padding: 5vh 5%;
+}
+
+#menu {
+  position: relative;
+}
+
+@media (max-width: 768px) {
+  nav {
+    display: none;
+    grid-column: 1fr;
+    margin-top: 1px;
+    position: absolute;
+    left: -100%;
+    top: 85px;
+    width: 200%;
+  }
+  .toggle:checked ~ nav {
+    display: grid;
+  }
+
+  .nav_link {
+    background-color: #F1EDE1;
+    border: 0.5px solid #A39D94;
+    margin: -1px;
+    padding-left: 3vw;
+  }
+
+  .mobile_menu, .toggle {
+    display: block;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+  #logo {
+    padding: 25px 0 18px 25px;
+  }
+}
+
+@media (max-width: 425px) {
+  h3 {
+    font-size: 1.2rem;
+  }
+  #logo {
+    padding: 30px 0 18px 25px;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,117 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        learned-equine
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="content_title">
+      <h1 class="title main">Learned Equine</h1>
+      <h4 class="title sub">It's pronounced "Learn-ed"</h4>
+    </div>
+    <div class="content">
+      <div class="text">
+        <div class="showcase_grid">
+          <div class="showcase1 grid_item img_wrapper">
+            <image class="img" src="~/assets/images/kellie.jpg" />
+          </div>
+          <div class="showcase2 grid_item img_wrapper">
+            <image class="img" src="~/assets/images/lita_3.jpg" />
+          </div>
+          <div class="showcase3 grid_item img_wrapper">
+            <image class="img reposition" src="~/assets/images/obi_2.jpg" />
+          </div>
+          <div class="showcase4 grid_item img_wrapper">
+            <image class="img" src="~/assets/images/parker_jovi_2.jpg" />
+          </div>
+          <div class="showcase5 grid_item img_wrapper">
+            <image class="img" src="~/assets/images/pumpkin.jpg" />
+          </div>
+          <div class="showcase6 grid_item img_wrapper">
+            <image class="img" src="~/assets/images/lita_2.jpg" />
+          </div>
+        </div>
+        <h2>Recent Posts</h2>
+        <ul>
+          
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+h2 {
   text-align: center;
 }
 
+ul {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.bg {
+  background-image: url('../assets/background/index.jpg');
+}
+
+.content_title {
+  margin-bottom: 55vh;
+  position: relative;
+  width: 100%;
+}
+
+.main {
+  font-family: Garamond;
+  font-size: 3rem;
+  font-weight: bold;
+}
+
+.sub {
+  font-style: italic;
+}
+
+.text {
+  padding: 5vh 5%;
+}
+
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  color: #FFFFFF;
+  text-align: center;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.showcase_grid {
+  align-items: stretch;
+  display: grid;
+  grid-template-areas:
+    'a a a a b b b b c c'
+    'a a a a b b b b c c'
+    'a a a a d d e e f f'
+    'a a a a d d e e f f';
+  grid-gap: 2px;
+  margin-bottom: 7vh;
+}
+.grid_item {
+  display: flex;
+  align-items: center;
+}
+.showcase1 { grid-area: a; }
+.showcase2 { grid-area: b; }
+.showcase3 { grid-area: c; }
+.showcase4 { grid-area: d; }
+.showcase5 { grid-area: e; }
+.showcase6 { grid-area: f; }
+
+.img {
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+}
+.reposition {
+  object-position: 0;
 }
 
-.links {
-  padding-top: 15px;
+@media (max-width: 425px) {
+  ul {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
